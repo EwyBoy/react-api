af947eaf81a16ebbbf41aa69568d776049628ad1
Ewy <EwyBoy@gmail.com>
2024-03-13 00:32:38 +0100

    Added sub-domain base

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: './dist',
-    assetsDir: '.',
-  }
+	plugins: [react()],
+	base: './',
+	build: {
+		outDir: './dist',
+		assetsDir: '.'
+	}
 })
